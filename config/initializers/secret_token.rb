6d5cd616d73cd1722c7a5d8f955c761b181490ea
Allan Davis <cajun.code@gmail.com>
2013-06-27 14:58:57 -0400

    Inital project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasktrak::Application.config.secret_key_base = '07fe81931ad0afef792b1b92b411775f9b25755e458699f76ad4bdffcdb658c83f44ea4f0c33ed065459e18e385578d68cfc639cab8faca9762dda79d6edfeac'
